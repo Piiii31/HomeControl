25135e7486f360b67c4c092dd16bc3773e451ede
Piiii31 <abderrahmen.othman@eniso.u-sousse.tn>
2024-10-05 12:38:14 +0100

    Update RenderState.ts

--- api/RenderState.ts
+++ api/RenderState.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetchPowerColumn = async (userId: string, deviceId: string) => {
     try {
-        const response = await axios.get(`https://djangobackend-seven.vercel.app/api/get_power_column/${userId}/${deviceId}`);
+        const response = await axios.get(`{put you url here }/api/get_power_column/${userId}/${deviceId}`);
         const data = response.data;
         return data.power_column && data.power_column.length > 0 ? data.power_column[0].clicked : '';
     } catch (error) {
